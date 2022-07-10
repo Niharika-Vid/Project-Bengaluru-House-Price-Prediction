a76d0821bad0008b9596414e3f8018e174186201
Niharika-Vid <108613146+Niharika-Vid@users.noreply.github.com>
2022-07-10 16:33:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function getBathValue() {
     var location = document.getElementById("uiLocations");
     var estPrice = document.getElementById("uiEstimatedPrice");
   
-     var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx 
+     var url = "https://github.com/Niharika-Vid/BHPP/predict_home_price"; //Use this if you are NOT using nginx 
      //var url = "/api/predict_home_price"; // Use this if  you are using nginx.
   
     $.post(url, {
@@ -43,7 +43,7 @@ function getBathValue() {
   
   function onPageLoad() {
     console.log( "document loaded" );
-    var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx 
+    var url = "https://github.com/Niharika-Vid/BHPP/get_location_names"; // Use this if you are NOT using nginx 
     //var url = "/api/get_location_names"; // Use this if  you are using nginx.
     $.get(url,function(data, status) {
         console.log("got response for get_location_names request");
